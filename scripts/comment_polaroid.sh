9bfe9d6eeb459f8e939cfeb94d9eeba9d118886a
Bruce Wayne <wouham@nemocorp.info>
2013-11-07 07:32:40 +0100

    Correction base.tmpl pour supprimer blog header en affichage telephone

--- scripts/comment_polaroid.sh
+++
@@ -1,5 +0,0 @@
-#convert $1 -background Lavender -font "Oregano" -pointsize 20 -gravity South label:"$2" -append -polaroid 0 $1
-
-convert -caption "$2" $1 -thumbnail '500x500>' \
-          -font  'Devonshire-Regular' -pointsize 24 -gravity center -bordercolor Lavender \
-          -background navy  -polaroid -0 commented_polaroid.png
