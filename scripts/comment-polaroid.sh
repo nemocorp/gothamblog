1ee9edf026803e529e0ec5229993e131c53f2124
Bruce Wayne <wouham@nemocorp.info>
2013-11-08 14:39:50 +0100

    Utilisation col-xs a la place de col-md sur la galerie

--- scripts/comment-polaroid.sh
+++ scripts/comment-polaroid.sh
@@ -1,5 +1,5 @@
 #convert $1 -background Lavender -font "Oregano" -pointsize 20 -gravity South label:"$2" -append -polaroid 0 $1
 
 convert -caption "$2" $1 -thumbnail '500x500>' \
-          -font  'Devonshire-Regular' -pointsize 24 -gravity center -bordercolor Lavender \
+          -font  'Annie-Use-Your-Telescope' -pointsize 28 -strokewidth 3 -gravity center -bordercolor Lavender \
           -background navy  -polaroid -0 commented_polaroid.png
