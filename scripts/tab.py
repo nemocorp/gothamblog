a4f07f269e8b5006e6fd9c9312dc2952e93a494d
Bruce Wayne <wouham@nemocorp.info>
2013-11-13 15:28:57 +0100

    Ajout fiche oddville & correction bug dans tab.py

--- scripts/tab.py
+++ scripts/tab.py
@@ -3,23 +3,22 @@ import sys
 import csv
 
 with open(sys.argv[1]) as src:
-    lines=csv.reader(src, delimiter=';')
-    max_length = 0
-    for i,l in enumerate(lines):
-        if len(l[0]) > max_length:
-            max_length = len(l[0])
+    lines=src.readlines()
+max_length = 0
+for l in lines:
+    if len(l) > max_length:
+        max_length = len(l)
 print(".. class::  well fiche pull-right")
 print()
 print ("\t+-%s-+" % ("-"*(max_length+1)))
 print ("\t| **Fiche** %s |" % (" "*(max_length - 9)))
 print ("\t+-%s-+" % ("-"*(max_length+1)))
-with open(sys.argv[1]) as src:
-    lines=csv.reader(src, delimiter=';')
-    for i,l in enumerate(lines):
-        if l[0]=='---':
-            print ("\t+-%s-+" % ("-"*(max_length+1)))
-        else:
-            print ("\t| %s %s |" % (l[0], " "*(max_length - len(l[0]))))
+for l in lines:
+    l=l.strip()
+    if l=='---':
+        print ("\t+-%s-+" % ("-"*(max_length+1)))
+    else:
+        print ("\t| %s %s |" % (l, " "*(max_length-len(l))))
 
 print ("\t+-%s-+" % ("-"*(max_length+1)))
 
