a2593b52c2ed2a6ddb6c72e79ad3b9d358d78ebc
batman <rutault.jp@gmail.com>
2013-11-18 13:04:00 +0100

    Ajout fiche Horreur à Arkham et BD Dômu

--- scripts/youtube.sh
+++ scripts/youtube.sh
@@ -4,5 +4,11 @@
 # $1: Image file
 # $2: Mp3 Song
 # 
+if [ $# -ne 2 ]
+then
+    echo "Usage: youtube.sh <cover.png> <music.mp3>"
+    exit
+fi
+
 convert -resize 250x "$1" "$1"
 ffmpeg -y -loop 1 -r 1 -i "$1" -i "$2" -acodec mp3 -shortest music-video.mp4
